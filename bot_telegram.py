2471ce3d5ba95852734dc9ef746f9c1a1b7195a2
ffructi <ffructifero@gmail.com>
2026-04-03 20:18:36 -0500

    bot

---
+++ bot_telegram.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import requests
+import sys
+
+# ==================== CONFIGURACIÓN ====================
+#   datos de Telegram
+BOT_TOKEN = "" 
+CHAT_ID = " "      # Ejemplo: "123456789"
+
+# Archivo CSV a procesar
+ARCHIVO_CSV =   # Cambiamos por la ruta de nuestro archivo
+
+COLUMNA_FILTRAR = "posted"  # Nombre de la columna 
+PALABRA_CLAVE = "horas"      # Palabra a buscar
+# =======================================================
+
